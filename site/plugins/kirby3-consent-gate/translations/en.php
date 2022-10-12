1bb347e1cb5a1b350d5994dd4d5adb8c9e71fa69
helloMikkie <mh@groenlandberlin.de>
2022-10-12 16:04:03 +0200

    consent

--- site/plugins/kirby3-consent-gate/translations/en.php
+++ site/plugins/kirby3-consent-gate/translations/en.php
@@ -3,8 +3,8 @@ return [
 	'fendinger.consent-gate.title' => 'Recommended external content',
 	'fendinger.consent-gate.consent' => 'We need yout consent to display the embedded content from { vendor }. You can display this content, and thus all other content from { vendor } on our website, with one click also deactivate it again.',
 	'fendinger.consent-gate.toggle_on' => 'Always show contents of { vendor }',
-	'fendinger.consent-gate.toggle_off' => 'Do not show content from { vendor } again',
-	'fendinger.consent-gate.privacy' => 'Privacy policy of { vendor }',
+	'fendinger.consent-gate.toggle_off' => 'Hide content from { vendor }',
+	'fendinger.consent-gate.privacy' => '{ vendor } Privacy policy',
 	'fendinger.consent-gate.agreement' => 'I agree to external content being displayed to me. This may transmit personal data to third-party platforms.',
 	'fendinger.consent-gate.more' => 'More about this in the',
 ];
